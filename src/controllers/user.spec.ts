de0a688d1f7f1bc5b923f84eb86e26dab8741c7b
Tarcisio Costa <tarcisiocorte@gmail.com>
2020-12-02 07:54:30 -0300

    test: test email and name

--- src/controllers/user.spec.ts
+++ src/controllers/user.spec.ts
@@ -15,4 +15,18 @@ describe('User Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no Email is provided', async () => {
+    const systemUnderTest = new UserController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = systemUnderTest.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
